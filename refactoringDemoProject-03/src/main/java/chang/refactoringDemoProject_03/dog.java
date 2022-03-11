396a020b504284a616166f6ac6683490bde504a2
chang <chang@192.168.1.107>
2022-03-11 14:53:38 +0800

    extract method 完成

--- refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/dog.java
+++ refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/dog.java
@@ -4,10 +4,14 @@ public class dog extends Animal{
 	public static void main(String[] args) {
 		dog dog =new dog();
 		int times=10;
+		wangWang(times);   
+	}
+
+	private static void wangWang(int times) {
 		for (int i = 0; i < times; i++) {
 			System.out.println("汪汪汪");
 			
-		}   
+		}
 	}
 
 	 
