888b30e517594aba65e9a6f7d7d8a6957bf8277a
chang <chang@192.168.1.107>
2022-03-11 14:39:54 +0800

    pull-down 完成

--- refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
+++ refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
@@ -2,9 +2,16 @@ package chang.refactoringDemoProject_02;
 
 public class dog extends Animal{ 
 	public static void main(String[] args) {
-		
+		Animal dog =new dog();
 		int times=10;
-		sayWangWang(times); 
+		dog.sayWangWang(times);  
+	}
+
+	public void sayWangWang(int times) {
+		for (int i = 0; i < times; i++) {
+			System.out.println("汪汪汪");
+			
+		}
 	}
 
 
