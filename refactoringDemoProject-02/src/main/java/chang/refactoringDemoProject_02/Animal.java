a0a9c422dc7396394b75c87927b159b74ecbcd55
chang <chang@192.168.1.107>
2022-03-11 14:38:01 +0800

    pull-up 初始

---
+++ refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/Animal.java
@@ -0,0 +1,5 @@
+package chang.refactoringDemoProject_02;
+
+public class Animal {
+	
+}
--- refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
+++ refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
@@ -4,10 +4,14 @@ public class dog {
 	public static void main(String[] args) {
 		
 		int times=10;
+		sayWangWang(times); 
+	}
+
+	private static void sayWangWang(int times) {
 		for (int i = 0; i < times; i++) {
 			System.out.println("汪汪汪");
 			
-		} 
+		}
 	}
 
 
