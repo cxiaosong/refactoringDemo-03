95fb2408d59bdbd2d47a7019994c00dd125c66b7
chang <chang@192.168.1.107>
2022-03-11 14:47:01 +0800

    rename 完成

--- refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
+++ refactoringDemoProject-02/src/main/java/chang/refactoringDemoProject_02/dog.java
@@ -4,10 +4,10 @@ public class dog extends Animal{
 	public static void main(String[] args) {
 		Animal dog =new dog();
 		int times=10;
-		dog.sayWangWang(times);  
+		dog.sayWangWangNew(times);  
 	}
 
-	public void sayWangWang(int times) {
+	public void sayWangWangNew(int times) { 
 		for (int i = 0; i < times; i++) {
 			System.out.println("汪汪汪");
 			
