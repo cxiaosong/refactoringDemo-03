879512de884d486ce8ae8b086189e2a1a8d60733
chang <chang@192.168.1.107>
2022-03-11 14:55:38 +0800

    pull-up method 完成

--- refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/Animal.java
+++ refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/Animal.java
@@ -1,5 +1,12 @@
 package chang.refactoringDemoProject_03;
 
 public class Animal {
+
+	private static void wangWang(int times) {
+		for (int i = 0; i < times; i++) {
+			System.out.println("汪汪汪"); 
+			
+		}
+	}
 	
 }
--- refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/dog.java
+++ refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/dog.java
@@ -2,19 +2,12 @@ package chang.refactoringDemoProject_03;
 
 public class dog extends Animal{ 
 	public static void main(String[] args) {
-		dog dog =new dog();
+		Animal dog =new dog();
 		int times=10;
 		for (int i = 0; i < times; i++) {
 			System.out.println("汪汪汪");
 			
 		}   
-	} 
-
-	private static void wangWang(int times) {
-		for (int i = 0; i < times; i++) {
-			System.out.println("汪汪汪"); 
-			
-		}
 	}
 
 	 
