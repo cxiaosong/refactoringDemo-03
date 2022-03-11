77aa34eb69d33879a80a91d81711b3fd466edc17
chang <chang@192.168.1.107>
2022-03-11 14:56:24 +0800

    pull-down method 完成

--- refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/Animal.java
+++ refactoringDemoProject-03/src/main/java/chang/refactoringDemoProject_03/Animal.java
@@ -2,11 +2,6 @@ package chang.refactoringDemoProject_03;
 
 public class Animal {
 
-	private static void wangWang(int times) {
-		for (int i = 0; i < times; i++) {
-			System.out.println("汪汪汪"); 
-			
-		}
-	}
+
 	
 }
